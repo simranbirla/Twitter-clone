fc78ba1f0932030ef78fff0ebcd909aa042011ce
simranbirla <ssbirla01@gmail.com>
2023-03-16 10:11:35 +0530

    fix: added more routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <div className="App">
-      Hello
-      <Navigation />
-    </div>
+    <Router>
+      <div className="App">
+        Hello
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<div>Heyyy</div>} />
+          <Route path="/about" element={<div>About</div>} />
+          <Route path="/contact" element={<div>Contact</div>} />
+          <Route
+            path="/profile/:id"
+            element={<div>Profile should be here</div>}
+          />
+          <Route path="/tweet/:id" element={<div>Single tweet display</div>} />
+          <Route path="/login" element={<div>Login</div>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,30 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<div>Heyyy</div>} />
-          <Route path="/about" element={<div>About</div>} />
-          <Route path="/contact" element={<div>Contact</div>} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
