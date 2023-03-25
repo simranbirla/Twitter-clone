6b052e42089ebeef49027eb3a5e7b9ed64758798
simranbirla <ssbirla01@gmail.com>
2023-03-25 12:13:21 +0530

    fix: add withcredentials to axios

--- client/src/utils/makeRequest.ts
+++ client/src/utils/makeRequest.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:3001",
+  withCredentials: true,
 });
 
 export const makeRequest = async (url: string, options?: any) => {
