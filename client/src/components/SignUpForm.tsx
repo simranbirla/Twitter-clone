6d24a64813e6a19dc4eb674cb67920d0f0154fc5
simranbirla <ssbirla01@gmail.com>
2023-03-30 12:51:57 +0530

    feat: signup form

--- client/src/components/SignUpForm.tsx
+++ client/src/components/SignUpForm.tsx
@@ -1,25 +1,30 @@
 import React, { useState } from "react";
+import { makeRequest } from "../utils/makeRequest";
+import { useNavigate } from "react-router-dom";
 
-type FormData = {
-  username: "";
-  email: "";
-  password: "";
-  confirmPassword: "";
-  name: "";
-  status: "";
+interface FormData {
+  username: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  name: string;
+  status: string;
+}
+
+const initialFormValues = {
+  username: "",
+  email: "",
+  password: "",
+  passwordConfirm: "",
+  name: "",
+  status: "",
 };
 
 export default function SignUpForm() {
-  const [formData, setFormData] = useState<FormData>({
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    name: "",
-    status: "",
-  });
+  const [formData, setFormData] = useState<FormData>(initialFormValues);
+  const [image, setImage] = useState<File | null>();
 
-  const [image, setImage] = useState();
+  const navigate = useNavigate();
 
   const handleFormData: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     setFormData((prevData) => ({
@@ -29,51 +34,83 @@ export default function SignUpForm() {
   };
 
   const handleFileChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    console.log(e.target.files);
+    setImage(e.target.files && e.target.files[0]);
   };
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
+    const { username, name, email, password, passwordConfirm } = formData;
     console.log(formData, image);
+    const formFieldData = new FormData();
+    formFieldData.append("username", username);
+    formFieldData.append("name", name);
+    formFieldData.append("email", email);
+    formFieldData.append("password", password);
+    formFieldData.append("passwordConfirm", passwordConfirm);
+    formFieldData.append("photo", image as Blob);
+
+    try {
+      const data = await makeRequest("/auth/signup", {
+        method: "POST",
+        data: formFieldData,
+      });
+
+      if (data.status === 201) navigate("/");
+    } catch (e) {
+      setFormData(initialFormValues);
+      setImage(undefined);
+    }
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
         <input
           type="text"
           name="username"
           placeholder="Enter Username"
           onChange={handleFormData}
+          value={formData.username}
+          required
         />
         <input
           type="text"
           name="name"
           placeholder="Enter name"
           onChange={handleFormData}
+          value={formData.name}
+          required
         />
         <input
           type="email"
           name="email"
           placeholder="Enter email"
           onChange={handleFormData}
+          value={formData.email}
+          required
         />
         <input
           type="file"
           name="image"
           placeholder="Select an image"
           onChange={handleFileChange}
+          required
         />
         <input
           type="password"
           name="password"
           placeholder="Enter password"
           onChange={handleFormData}
+          value={formData.password}
+          required
         />
         <input
           type="password"
-          name="confirmPassword"
+          name="passwordConfirm"
           placeholder="Confirm password"
           onChange={handleFormData}
+          value={formData.passwordConfirm}
+          required
         />
         <button type="submit">Submit</button>
       </form>
